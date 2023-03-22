05dd1c167a4b21bc374f357d7740c905e8eaf6c0
sensationsolution <ashu.khan0077@gmail.com>
2023-03-22 16:08:42 +0530

    changes

--- SocialMedia/Program.cs
+++ SocialMedia/Program.cs
@@ -5,66 +5,68 @@ using SocialMedia.Repository.EF.Context;
 using SocialMedia.Repository.Interfaces;
 using SocialMedia.Repository.Repositories;
 using SocialMedia.Repository.Repository;
-using SocialMedia.Service.Package;
+using SocialMedia.Service.Services.Master;
+using SocialMedia.Service.Services.Package;
 using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-
-builder.Services.AddDbContext<AppDbContext>(options =>
+try
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionString"));
-});
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionString"));
+    });
 
 
-builder.Services.AddCors(o =>
-{
-    o.AddPolicy(
-        "MyPolicy",
-        builder =>
-        {
-            builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
-        });
-});
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+    builder.Services.AddCors(o =>
+    {
+        o.AddPolicy(
+            "MyPolicy",
+            builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+            });
+    });
+    builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-builder.Configuration.AddEnvironmentVariables();
-builder.Services.AddScoped<IPackageService, PackageService>();
-builder.Services.AddScoped<IMasterCategoryRepository, MasterCategoryRepository>();
-builder.Services.AddScoped<IMasterSubCategoryRepository, MasterSubCategoryRepository>();
-builder.Services.AddScoped<IMasterFeatureRepository, MasterFeatureRepository>();
-
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();  
-
-
-// Automatically add IoC interfaces
-
+    builder.Configuration.AddEnvironmentVariables();
+    builder.Services.AddScoped<IPackageService, PackageService>();
+    builder.Services.AddScoped<IMasterCategoryRepository, MasterCategoryRepository>();
+    builder.Services.AddScoped<IMasterSubCategoryRepository, MasterSubCategoryRepository>();
+    builder.Services.AddScoped<IMasterFeatureRepository, MasterFeatureRepository>();
+    builder.Services.AddScoped<IMasterService, MasterService>();
+    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 
 
-// Add services to the container.
+    // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+    app.UseCors("MyPolicy");
+    app.Run();
 }
+catch (Exception ex)
+{
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-app.UseCors("MyPolicy");
-app.Run();
+}
